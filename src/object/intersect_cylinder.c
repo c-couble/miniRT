1befca4d506d59a7b8fcd9677335f7e23c3f8f18
ccouble <ccouble@student.42lyon.fr>
2024-06-11 22:40:58 +0200

    restructure cylinder

--- src/object/intersect_cylinder.c
+++ src/object/intersect_cylinder.c
@@ -6,7 +6,7 @@
 /*   By: ccouble <ccouble@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 22:57:46 by ccouble           #+#    #+#             */
-/*   Updated: 2024/06/11 20:30:35 by ccouble          ###   ########.fr       */
+/*   Updated: 2024/06/11 22:34:09 by ccouble          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,15 +23,15 @@ static double	check_disk(t_object *obj, t_ray *ray, t_vec3 *p);
 
 double	intersect_cylinder(t_object *obj, t_ray *ray)
 {
-	double	t = hit_cyl(obj, ray);
-
-	t_vec3	a = obj->data.cylinder.axis;
-	vec3_scale(&a, obj->data.cylinder.height / 2);
-	t_vec3	r2;
-	r2 = obj->data.cylinder.coordinates;
-	vec3_add(&r2, &a, &r2);
-
+	double	t;
+	t_vec3	a;
 	t_vec3	r1;
+	t_vec3	r2;
+
+	a = obj->data.cylinder.axis;
+	t = hit_cyl(obj, ray);
+	vec3_scale(&a, obj->data.cylinder.height / 2);
+	vec3_add(&obj->data.cylinder.coordinates, &a, &r2);
 	vec3_subtract(&obj->data.cylinder.coordinates, &a, &r1);
 	t = get_closest_distance(t, check_disk(obj, ray, &r1));
 	t = get_closest_distance(t, check_disk(obj, ray, &r2));
@@ -56,37 +56,38 @@ static double	check_disk(t_object *obj, t_ray *ray, t_vec3 *p)
 	return (-1);
 }
 
+static void	solve_cylinder_quadratic(t_object *obj, t_ray *ray, t_quadratic *q)
+{
+	t_vec3	ra0;
+	t_vec3	va;
+	t_vec3	diff;
+
+	vec3_subtract(&ray->startpos, &obj->data.cylinder.coordinates, &diff);
+	vec3_cross_product(&obj->data.cylinder.axis, &diff, &ra0);
+	vec3_cross_product(&ra0, &obj->data.cylinder.axis, &ra0);
+	vec3_cross_product(&obj->data.cylinder.axis, &ray->ray, &va);
+	vec3_cross_product(&va, &obj->data.cylinder.axis, &va);
+	q->a = vec3_dot_product(&va, &va);
+	q->c = (vec3_dot_product(&ra0, &ra0) - powl(obj->data.cylinder.diameter / 2, 2));
+	vec3_scale(&ra0, 2);
+	q->b = vec3_dot_product(&ra0, &va);
+	solve_quadratic_equation(q);
+}
+
 static double	hit_cyl(t_object *obj, t_ray *ray)
 {
 	t_quadratic	q;
 	ray->color = obj->data.cylinder.color;
-	t_vec3	a = obj->data.cylinder.axis;
-	vec3_scale(&a, obj->data.cylinder.height / 2);
-	t_vec3	r2;
-	r2 = obj->data.cylinder.coordinates;
-	vec3_add(&r2, &a, &r2);
-
-	t_vec3	r1;
-	vec3_subtract(&obj->data.cylinder.coordinates, &a, &r1);
-	t_vec3	var1;
-	vec3_subtract(&ray->startpos, &r1, &var1);
-
-	t_vec3	a0;
-	a0 = obj->data.cylinder.axis;
-	vec3_cross_product(&obj->data.cylinder.axis, &var1, &a0);
-	vec3_cross_product(&a0, &obj->data.cylinder.axis, &a0);
-
-	t_vec3	va;
-	vec3_cross_product(&obj->data.cylinder.axis, &ray->ray, &va);
-	vec3_cross_product(&va, &obj->data.cylinder.axis, &va);
-	q.a = vec3_dot_product(&va, &va);
-	q.c = (vec3_dot_product(&a0, &a0) - powl(obj->data.cylinder.diameter / 2, 2));
-	vec3_scale(&a0, 2);
-	q.b = vec3_dot_product(&a0, &va);
-
-	solve_quadratic_equation(&q);
+	solve_cylinder_quadratic(obj, ray, &q);
 	if (q.delta >= 0)
 	{
+		t_vec3	r1;
+		t_vec3	a = obj->data.cylinder.axis;
+		vec3_scale(&a, obj->data.cylinder.height / 2);
+		vec3_subtract(&obj->data.cylinder.coordinates, &a, &r1);
+		t_vec3	r2;
+		r2 = obj->data.cylinder.coordinates;
+		vec3_add(&r2, &a, &r2);
 		t_vec3	rray;
 		rray = ray->startpos;
 		t_vec3	rayt;
