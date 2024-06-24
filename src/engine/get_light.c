492c60c8fa109972fddcdd4b0e1ab98a21ce6d63
lespenel <leoespenel@hotmail.com>
2024-06-24 10:17:28 +0200

    light

--- src/engine/get_light.c
+++ src/engine/get_light.c
@@ -6,7 +6,7 @@
 /*   By: ccouble <ccouble@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/15 02:12:48 by ccouble           #+#    #+#             */
-/*   Updated: 2024/06/24 03:32:31 by lespenel         ###   ########.fr       */
+/*   Updated: 2024/06/24 05:23:50 by lespenel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 #include <stdio.h>
 
 static void	mix_lights(t_color *c1, t_color *c2, double ratio);
-static int	hits_light(t_engine *engine, t_ray *ray, t_object *obj);
+static int	hits_light(t_engine *engine, t_ray *light_ray, t_ray *ray, t_object *obj);
 static void	difuse_reflect(t_color *light, t_vec3 *obj_n, t_vec3 *light_n);
 static void	specular_reflect(t_color *light, t_vec3 *obj_n, t_vec3 *light_n, t_ray *ray);
 
@@ -30,6 +30,7 @@ t_color	get_light(t_engine *engine, t_ray *ray)
 	size_t		i;
 	t_object	*obj;
 	t_vec3		object_n;
+	t_ray		light_ray;
 
 	light.color = 0;
 	i = 0;
@@ -39,11 +40,11 @@ t_color	get_light(t_engine *engine, t_ray *ray)
 		obj = at_vector(&engine->scene.objects, i);
 		if (obj->type == LIGHT)
 		{
-			if (hits_light(engine, ray, obj))
+			if (hits_light(engine, &light_ray, ray, obj))
 			{
 				light2 = obj->data.light.color;
-				difuse_reflect(&light2, &object_n, &ray->ray);
-				specular_reflect(&light2, &object_n, &ray->ray, ray);
+				difuse_reflect(&light2, &object_n, &light_ray.ray);
+				specular_reflect(&light2, &object_n, &light_ray.ray, ray);
 				mix_lights(&light, &light2, 1);
 			}
 		}
@@ -54,28 +55,31 @@ t_color	get_light(t_engine *engine, t_ray *ray)
 	return (light);
 }
 
-static void	specular_reflect(t_color *light, t_vec3 *obj_n, t_vec3 *light_n, t_ray *ray)
+// R = 2(N.L)N - L
+static void	specular_reflect(t_color *light, t_vec3 *obj_n, t_vec3 *light_ray, t_ray *ray)
 {
 	double	specular_ratio;
-
 	t_vec3	reflection_ray;
-	double	dot_l_n;
+	double	dot_N_L;
+	double	shine = 10;
 
-	dot_l_n = vec3_dot_product(obj_n, light_n);
-	light_n = vec3_scale(obj_n, 2 *  dot_l_n);
-	vec3_subtract(obj_n, light_n, &reflection_ray);
+
+	t_vec3 *temp_N = vec3_scale(obj_n, 1);
+	dot_N_L = vec3_dot_product(temp_N, light_ray) * 2;
+	vec3_scale(temp_N, dot_N_L);
+	vec3_subtract(temp_N, light_ray, &reflection_ray);
+	vec3_scale(&ray->ray, -1);
 	specular_ratio = vec3_dot_product(&reflection_ray, &ray->ray);
-	double shine = 2;
 
-	specular_ratio = pow(dot_l_n, shine);
-	printf("specular ratio = %lf", specular_ratio);
+	specular_ratio = pow(specular_ratio, shine);
+	//printf("specular ratio = %lf", specular_ratio);
 	light->rgb.r *= specular_ratio;
 	light->rgb.g *= specular_ratio;
 	light->rgb.b *= specular_ratio;
 	(void)specular_ratio;
 	(void)light;
 	(void)obj_n;
-	(void)light_n;
+	(void)light_ray;
 	(void)ray;
 }
 
@@ -83,6 +87,11 @@ static void	difuse_reflect(t_color *light, t_vec3 *obj_n, t_vec3 *light_n)
 {
 	double	ratio;
 
+	(void)light_n;
+	(void)obj_n;
+	(void)light;
+	(void)ratio;
+	/*
 	ratio = vec3_dot_product(light_n, obj_n);
 	ratio = ft_double_abs(ratio);
 	light->rgb.r *= ratio;
@@ -90,6 +99,7 @@ static void	difuse_reflect(t_color *light, t_vec3 *obj_n, t_vec3 *light_n)
 	light->rgb.b *= ratio;
 	if (light < 0)
 		light = 0;
+	*/
 }
 
 static void	mix_lights(t_color *c1, t_color *c2, double ratio)
@@ -99,14 +109,14 @@ static void	mix_lights(t_color *c1, t_color *c2, double ratio)
 	c1->rgb.b = ft_max(c1->rgb.b, c2->rgb.b * ratio);
 }
 
-static int	hits_light(t_engine *engine, t_ray *ray, t_object *obj)
+static int	hits_light(t_engine *engine, t_ray *light_ray, t_ray *ray, t_object *obj)
 {
 	double	norm;
 	double	d;
 
-	ray->startpos = ray->data.hitpos;
-	vec3_subtract(&obj->data.light.pos, &ray->startpos, &ray->ray);
-	norm = vec3_normalize(&ray->ray);
-	d = trace_ray(engine, ray);
+	light_ray->startpos = ray->data.hitpos;
+	vec3_subtract(&obj->data.light.pos, &light_ray->startpos, &light_ray->ray);
+	norm = vec3_normalize(&light_ray->ray);
+	d = trace_ray(engine, light_ray);
 	return (d < 0 || d > norm);
 }
