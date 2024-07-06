532a96822922daaed7e4087d9ed020484d5468cb
lespenel <leoespenel@hotmail.com>
2024-07-06 02:13:26 +0200

    engine.h

--- include/engine.h
+++ include/engine.h
@@ -6,7 +6,7 @@
 /*   By: ccouble <ccouble@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/12 19:52:01 by ccouble           #+#    #+#             */
-/*   Updated: 2024/06/15 02:14:43 by ccouble          ###   ########.fr       */
+/*   Updated: 2024/07/06 02:01:36 by lespenel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int		init_engine(t_engine *engine, char *scene);
 void	clear_engine(t_engine *engine);
 void	render_frame(t_engine *engine);
 struct	s_ray;
-t_color	get_light(t_engine *engine, struct s_ray *ray);
+uint32_t	get_light(t_engine *engine, struct s_ray *ray);
 void	run_loop(t_engine *engine);
 
 #endif
