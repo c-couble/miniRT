50fd97e096d9da0ae93818abb0095f45b454804c
ccouble <ccouble@student.42lyon.fr>
2024-07-20 04:08:20 +0200

    add 3d obj pointer to mesh obj

--- include/object/mesh.h
+++ include/object/mesh.h
@@ -6,18 +6,20 @@
 /*   By: ccouble <ccouble@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/20 03:43:40 by ccouble           #+#    #+#             */
-/*   Updated: 2024/07/20 04:05:09 by ccouble          ###   ########.fr       */
+/*   Updated: 2024/07/20 04:06:27 by ccouble          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MESH_H
 # define MESH_H
 
+# include "obj_3d.h"
 # include "vec3.h"
 
 typedef struct s_mesh
 {
-	t_vec3	pos;
+	t_obj_3d	*obj_3d;
+	t_vec3		pos;
 }	t_mesh;
 
 #endif
