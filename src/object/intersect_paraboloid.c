9ad51313a4f3e9fa5904127645ce4de1d81e7a07
lespenel <leoespenel@hotmail.com>
2024-08-07 03:38:21 +0200

    norm

--- src/object/intersect_paraboloid.c
+++ src/object/intersect_paraboloid.c
@@ -6,7 +6,7 @@
 /*   By: lespenel <lespenel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 01:00:41 by lespenel          #+#    #+#             */
-/*   Updated: 2024/08/07 01:39:50 by lespenel         ###   ########.fr       */
+/*   Updated: 2024/08/07 03:38:02 by lespenel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,78 +18,76 @@
 #include "util.h"
 #include "ft_math.h"
 
-int	check_height(t_ray *ray, t_paraboloid *para);
-static void	get_paraboloid_normal(t_ray *ray, t_paraboloid *paraboloid);
-
-static void	get_paraboloid_normal(t_ray *ray, t_paraboloid *paraboloid)
-{
-	ray->data.normal.x = 2 * paraboloid->ray_coef
-		* (ray->data.hitpos.x);
-	ray->data.normal.y = 2 * paraboloid->ray_coef
-		* (ray->data.hitpos.y);
-	ray->data.normal.z = -1;
-	vec3_normalize(&ray->data.normal);
-}
-
-double	solve_paraboloid(t_vec3 *origin, t_vec3 *dir, t_ray *ray, t_object *obj)
-{
-	t_quadratic	q;
-	double		t;
-	double		a;
-
-	a = obj->data.paraboloid.ray_coef;
-	q.a = a * (dir->x * dir->x + dir->y * dir->y);
-	q.b = a * 2 * (origin->x * dir->x + origin->y * dir->y) - dir->z;
-	q.c = a * (origin->x * origin->x + origin->y * origin->y) - origin->z;
-	solve_quadratic_equation(&q);
-	if (q.delta < 0)
-		return (-1);
-	t = get_closest_distance(q.r1, q.r2);
-	if (t == -1)
-		return (-1);
-	get_hitpos(ray, t);
-	if (check_height(ray, &obj->data.paraboloid))
-	{
-		t = get_bigest_distance(q.r1, q.r2);
-		get_hitpos(ray, t);
-		if (check_height(ray, &obj->data.paraboloid))
-			return (-1);
-	}
-	get_hitpos(ray, t);
-	get_paraboloid_normal(ray, &obj->data.paraboloid);
-	if (vec3_dot_product(&ray->data.normal, &ray->ray) > 0)
-		vec3_scale(&ray->data.normal, -1);
-	return (t);
-}
+static int		check_height(t_ray *ray, t_paraboloid *para);
+static void		get_paraboloid_normal(t_ray *ray, t_paraboloid *paraboloid);
+static double	solve_para(t_vec3 *o, t_vec3 *d, t_ray *r, t_object *obj);
 
 double	intersect_paraboloid(t_object *obj, t_ray *ray)
 {
 	const t_paraboloid	para = obj->data.paraboloid;
-	t_ray				offset_ray;
+	t_ray				obj_ray;
 	double				t;
 
-	offset_ray = *ray;
-	vec3_subtract(&ray->startpos, (t_vec3 *)&para.pos, &offset_ray.startpos);
-	quaternion_rotate(&offset_ray.startpos,
-		(t_vec3 *)&para.rot_axis, para.theta, &offset_ray.startpos);
+	obj_ray = *ray;
+	vec3_subtract(&ray->startpos, (t_vec3 *)&para.pos, &obj_ray.startpos);
+	quaternion_rotate(&obj_ray.startpos,
+		(t_vec3 *)&para.rot_axis, para.theta, &obj_ray.startpos);
 	quaternion_rotate(&ray->ray, (t_vec3 *)&para.rot_axis,
-		para.theta, &offset_ray.ray);
-	t = solve_paraboloid(&offset_ray.startpos, &offset_ray.ray, &offset_ray, obj);
-	quaternion_rotate(&offset_ray.data.normal, (t_vec3 *)
+		para.theta, &obj_ray.ray);
+	t = solve_para(&obj_ray.startpos, &obj_ray.ray, &obj_ray, obj);
+	if (t == -1)
+		return (-1);
+	get_paraboloid_normal(&obj_ray, &obj->data.paraboloid);
+	if (vec3_dot_product(&obj_ray.data.normal, &ray->ray) > 0)
+		vec3_scale(&obj_ray.data.normal, -1);
+	quaternion_rotate(&obj_ray.data.normal, (t_vec3 *)
 		&para.rot_axis, -para.theta, &ray->data.normal);
 	get_hitpos(ray, t);
 	ray->data.color = obj->data.paraboloid.color;
 	return (t);
 }
 
-int	check_height(t_ray *ray, t_paraboloid *para)
+static double	solve_para(t_vec3 *start, t_vec3 *dir, t_ray *ray, t_object *o)
 {
-	t_vec3		origin_hit;
-	t_vec3		axis = {0,0,1};
-	t_vec3		pos = {0,0,0};
+	t_quadratic	q;
+	double		t;
+	double		a;
 
-	vec3_subtract(&ray->data.hitpos, &pos, &origin_hit);
-	if (ft_dabs(vec3_dot_product(&axis, &origin_hit)) > para->height)
+	a = o->data.paraboloid.ray_coef;
+	q.a = a * (dir->x * dir->x + dir->y * dir->y);
+	q.b = a * 2 * (start->x * dir->x + start->y * dir->y) - dir->z;
+	q.c = a * (start->x * start->x + start->y * start->y) - start->z;
+	solve_quadratic_equation(&q);
+	if (q.delta < 0)
+		return (-1);
+	t = get_closest_distance(q.r1, q.r2);
+	get_hitpos(ray, t);
+	if (check_height(ray, &o->data.paraboloid))
+	{
+		t = get_bigest_distance(q.r1, q.r2);
+		get_hitpos(ray, t);
+		if (check_height(ray, &o->data.paraboloid))
+			return (-1);
+	}
+	return (t);
+}
+
+static int	check_height(t_ray *ray, t_paraboloid *para)
+{
+	t_vec3			origin_hit;
+	const t_vec3	axis = {0, 0, 1};
+	const t_vec3	pos = {0, 0, 0};
+
+	vec3_subtract(&ray->data.hitpos, (t_vec3 *)&pos, &origin_hit);
+	if (ft_dabs(vec3_dot_product((t_vec3 *)&axis, &origin_hit)) > para->height)
 		return (-1);
 	return (0);
+}
+
+static void	get_paraboloid_normal(t_ray *ray, t_paraboloid *para)
+{
+	ray->data.normal.x = 2 * para->ray_coef * (ray->data.hitpos.x);
+	ray->data.normal.y = 2 * para->ray_coef * (ray->data.hitpos.y);
+	ray->data.normal.z = -1;
+	vec3_normalize(&ray->data.normal);
 }
