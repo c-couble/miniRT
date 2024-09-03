eacd79eb21dda01e73136759ad9b167e6caa8f48
lespenel <leoespenel@hotmail.com>
2024-09-03 11:23:26 +0200

    get_uv in interset_paraboloid

--- src/object/intersect_paraboloid.c
+++ src/object/intersect_paraboloid.c
@@ -6,10 +6,11 @@
 /*   By: lespenel <lespenel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 01:00:41 by lespenel          #+#    #+#             */
-/*   Updated: 2024/09/02 09:47:15 by lespenel         ###   ########.fr       */
+/*   Updated: 2024/09/03 11:22:01 by lespenel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <math.h>
 #include "ft_math.h"
 #include "math_util.h"
 #include "object.h"
@@ -17,11 +18,11 @@
 #include "ray.h"
 #include "util.h"
 #include "vec3.h"
-#include <math.h>
 
 static int		check_height(t_ray *ray, t_paraboloid *para);
 static void		get_paraboloid_normal(t_ray *ray, t_paraboloid *paraboloid);
 static double	solve_para(t_vec3 *o, t_vec3 *d, t_ray *r, t_object *obj);
+static void		get_uv(t_ray *obj_ray, t_ray *ray, const t_paraboloid *para);
 
 double	intersect_paraboloid(t_object *obj, t_ray *ray)
 {
@@ -42,16 +43,9 @@ double	intersect_paraboloid(t_object *obj, t_ray *ray)
 	quaternion_rotate(&obj_ray.data.normal, (t_vec3 *)
 		&para.rot_axis, -para.theta, &ray->data.normal);
 	get_hitpos(ray, t);
+	ray->data.texture = obj->optional_data.texture;
 	ray->data.color = obj->data.paraboloid.color;
-	/*
-	if (ray->data.texture)
-	{
-		double thetha = atan2(obj_ray.data.hitpos.y, obj_ray.data.hitpos.x);
-		ray->data.u = thetha / (2 * M_PI);
-		if (ray->data.u < 0)
-			ray->data.u += 1.0;
-		ray->data.v = obj_ray.data.hitpos.z / obj->data.paraboloid.height;
-	}*/
+	get_uv(&obj_ray, ray, &para);
 	return (t);
 }
 
@@ -98,4 +92,18 @@ static void	get_paraboloid_normal(t_ray *ray, t_paraboloid *para)
 	ray->data.normal.y = 2 * para->ray_coef * (ray->data.hitpos.y);
 	ray->data.normal.z = -1;
 	vec3_normalize(&ray->data.normal);
+}
+
+static void	get_uv(t_ray *obj_ray, t_ray *ray, const t_paraboloid *para)
+{
+	double	thetha;
+
+	if (ray->data.texture)
+	{
+		thetha = atan2(obj_ray->data.hitpos.y, obj_ray->data.hitpos.x);
+		ray->data.u = thetha / (2 * M_PI);
+		if (ray->data.u < 0)
+			ray->data.u += 1.0;
+		ray->data.v = obj_ray->data.hitpos.z / para->height;
+	}
 }
