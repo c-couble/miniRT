48821e8d78dbeba3e99d3367cbb437c0bb57549b
ccouble <ccouble@student.42lyon.fr>
2024-09-27 05:10:32 +0200

    fix mtl parser

--- src/obj_3d/mtl_lines/parse_ni.c
+++ src/obj_3d/mtl_lines/parse_ni.c
@@ -6,7 +6,7 @@
 /*   By: ccouble <ccouble@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 04:53:04 by ccouble           #+#    #+#             */
-/*   Updated: 2024/09/25 04:53:36 by ccouble          ###   ########.fr       */
+/*   Updated: 2024/09/27 05:03:22 by ccouble          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,6 @@ int	parse_ni(t_engine *engine, t_obj_mtl *mtl, char *line)
 	(void) engine;
 	material = at_vector(&mtl->materials, mtl->materials.size - 1);
 	word = ft_strtok_r(line, " \t", &save);
+	return (0);
 	return (parse_double(&material->refraction_ratio, word, 0, 10));
 }
