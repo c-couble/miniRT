e9a9faf74e9d27c9dfa24176274679528533c8d0
lespenel <leoespenel@hotmail.com>
2024-09-28 05:54:51 +0200

    norm

--- include/bvh.h
+++ include/bvh.h
@@ -6,13 +6,14 @@
 /*   By: lespenel <lespenel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 13:31:59 by lespenel          #+#    #+#             */
-/*   Updated: 2024/09/28 05:30:43 by lespenel         ###   ########.fr       */
+/*   Updated: 2024/09/28 05:51:51 by lespenel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BVH_H
 # define BVH_H
 
+# include "ray.h"
 # include "object/triangle.h"
 # include "bounding_box.h"
 # include "vector.h"
@@ -41,7 +42,6 @@ typedef struct s_bvh
 	t_bvh_mode	bvh_mode;
 }	t_bvh;
 
-struct	s_ray;
 struct	s_engine;
 
 int			init_bvh(t_bvh *bvh, t_vector *objs);
@@ -52,13 +52,12 @@ t_bvh_node	*init_bvh_tree(t_vector *objs);
 t_bvh_node	*init_bvh_mesh(struct s_cached_triangle *tri, size_t size);
 void		clear_bvh_tree(t_bvh_node *bvh);
 void		update_node_aabb(t_bvh_node *node, t_vector *objs);
-void		update_node_aabb_mesh(t_bvh_node *node, struct s_cached_triangle *tris);
+void		update_node_aabb_mesh(t_bvh_node *n, struct s_cached_triangle *t);
 void		swap_by_axis(t_bvh_node *bvh, t_vector *objs, double pos, int axis);
 void		swap_tris(t_bvh_node *node, t_cached_triangle *t, double p, int a);
 double		get_split_pos_axis(t_bvh_node *node, int *axis);
-double		intersect_bvh(struct s_ray *ray, t_bvh_node *node, t_vector *objs);
-double		intersect_bvh_mesh(struct s_ray *ray, t_bvh_node *node,
-		struct s_cached_triangle *tris);
+double		intersect_bvh(t_ray *ray, t_bvh_node *node, t_vector *objs);
+double		intersect_bvh_mesh(t_ray *r, t_bvh_node *n, t_cached_triangle *t);
 
 void		get_bvh_depth(t_bvh_node *bvh, int depth, int *depth_ptr);
 void		print_node(t_bvh_node *n, t_vector *objs, char *str);
--- include/object/triangle.h
+++ include/object/triangle.h
@@ -6,14 +6,14 @@
 /*   By: lespenel <lespenel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/09 23:00:08 by lespenel          #+#    #+#             */
-/*   Updated: 2024/09/28 01:36:17 by lespenel         ###   ########.fr       */
+/*   Updated: 2024/09/28 05:50:01 by lespenel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TRIANGLE_H
 # define TRIANGLE_H
 
-#include "bounding_box.h"
+# include "bounding_box.h"
 # include "color.h"
 # include "object/material.h"
 # include "texture.h"
