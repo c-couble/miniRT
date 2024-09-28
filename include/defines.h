69f30788ebc65dccef0fb5e3bb668a2e5dfede40
lespenel <leoespenel@hotmail.com>
2024-09-29 01:26:31 +0200

    norm

--- include/defines.h
+++ include/defines.h
@@ -6,15 +6,15 @@
 /*   By: lespenel <lespenel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 04:21:31 by lespenel          #+#    #+#             */
-/*   Updated: 2024/09/28 13:48:54 by lespenel         ###   ########.fr       */
+/*   Updated: 2024/09/29 01:26:16 by lespenel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef DEFINES_H
 # define DEFINES_H
 
-# define SCREEN_WIDTH		1920 / 2
-# define SCREEN_HEIGHT		1080 / 2
+# define SCREEN_WIDTH		1920
+# define SCREEN_HEIGHT		1080
 # define WINDOW_NAME		"miniRT ccouble lespenel"
 # define DEFAULT_RAY_SIZE	5
 # define MINIMUM_FPS		20
