d28680d0f39ca5564906ceb7e01304d14234980c
ccouble <ccouble@student.42lyon.fr>
2024-09-30 05:00:11 +0200

    Add normals to cached triangle

--- src/obj_3d/build_obj_cache.c
+++ src/obj_3d/build_obj_cache.c
@@ -6,7 +6,7 @@
 /*   By: ccouble <ccouble@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/05 02:53:25 by ccouble           #+#    #+#             */
-/*   Updated: 2024/09/29 04:47:02 by ccouble          ###   ########.fr       */
+/*   Updated: 2024/09/30 04:59:53 by ccouble          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,6 +77,16 @@ static void	cache_point(t_mesh *mesh, t_mat4 *mat, size_t face, int i)
 		c->point_tx[i] = NULL;
 	else
 		c->point_tx[i] = at_vector(&obj->tx_coords, p->points[i].tx_id - 1);
+	if (p->points[i].normal_id == (size_t) -1)
+		vec3_create(0, 0, 0, c->normals + i);
+	else
+	{
+		vec4_create(at_vector(&obj->vertices, p->points[i].normal_id - 1), 1, &v4);
+		vec4_mat4_mult(&v4, mat, &v4);
+		c->normals[i].x = v4.x;
+		c->normals[i].y = v4.y;
+		c->normals[i].z = v4.z;
+	}
 }
 
 static void	finish_polygon(t_cached_triangle *t)
@@ -85,4 +95,9 @@ static void	finish_polygon(t_cached_triangle *t)
 	vec3_subtract(&t->points[2], &t->points[0], &t->e2);
 	vec3_cross(&t->e1, &t->e2, &t->normal);
 	vec3_normalize(&t->normal);
+	if (vec3_get_norm(&t->normals[0]) != 0
+		&& vec3_get_norm(&t->normals[1]) != 0 && vec3_get_norm(&t->normals[0]) != 0)
+		t->has_normals = 1;
+	else
+		t->has_normals = 0;
 }
